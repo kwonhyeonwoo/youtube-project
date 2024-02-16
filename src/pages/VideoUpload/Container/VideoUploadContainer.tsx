c0ef2d4e2ea3d253130dfceebc3493c4107cc6ef
woo <khw19980926@gmail.com>
2024-02-16 17:54:17 +0900

    비디오 업로드 개발

--- src/pages/VideoUpload/Container/VideoUploadContainer.tsx
+++ src/pages/VideoUpload/Container/VideoUploadContainer.tsx
@@ -1,6 +1,57 @@
+import React, { useState } from "react";
 import VideoUpload from "../VideoUpload"
-
+type Data ={
+    title:string;
+    description:string;
+    hashtags:string[];
+}
 const VideoUploadContainer = ()=>{
+    const [videoUrl, setVideoUrl] = useState<File | null>(null);
+    const [videoData, setVideoData] = useState({
+        title:"",
+        description:"",
+        hashtags:[""],
+    })
+    const VideoDataChange = (event:React.ChangeEvent<HTMLInputElement>)=>{
+        const {name, value,files}=event.target;
+        if(name === 'title'){
+            setVideoData(current=>({
+                ...current,
+                title:value
+            }));
+        }
+        if (name === 'description') {
+            setVideoData(current => ({
+                ...current,
+                description: value
+            }));
+        };
+        if (name === 'hashtags') {
+            setVideoData(current => ({
+                ...current,
+                hashtags: [value]
+            }));
+        };
+        if (name === 'videoUrl' && files && files.length > 0) {
+            setVideoUrl(files[0]);
+        }
+    }
+    const VideoUploadSubmit =async (event: React.MouseEvent<HTMLButtonElement>)=>{
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('title', videoData.title)
+        formData.append('description', videoData.description)
+        videoData.hashtags.forEach((item,idx)=>{
+            formData.append(`hashtags[${idx}]`,item)
+        })
+        formData.append('videoUrl',videoUrl as Blob);
+        const response = await fetch("http://localhost:4000/video/upload",{
+            method:"POST",
+            body:formData
+        })
+        const responseData = await response.json();
+        console.log('responseData',responseData);
+    }
     return(
         <VideoUpload/>
     )
